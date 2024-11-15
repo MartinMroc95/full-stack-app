b3bffdc0f9c58baf8e9dff4d5d7d3e41865c70c2
Martin Mroč <mato.mroc@gmail.com>
2024-11-15 12:58:16 +0100

    feat: show email in sidebar of singed in user

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,24 +1,35 @@
 import React from 'react'
-import { HStack } from '@chakra-ui/react'
+import { useUser } from '@auth0/nextjs-auth0/client'
+import { HStack, Text, VStack } from '@chakra-ui/react'
 import Link from 'next/link'
 
 interface Props {
   children: React.ReactNode
 }
 
-export const Layout: React.FC<Props> = ({ children }) => (
-  <HStack gap="10px" h="100vh" spacing={0}>
-    <HStack
-      zIndex="sticky"
-      justifyContent="center"
-      minW="90px"
-      h="full"
-      py="30px"
-      borderRadius="0px 25px 25px 0px"
-      bgColor="wheat"
-    >
-      <Link href="/api/auth/logout">Logout</Link>
+export const Layout: React.FC<Props> = ({ children }) => {
+  const { user } = useUser()
+
+  return (
+    <HStack gap="10px" h="100vh" spacing={0}>
+      <VStack
+        zIndex="sticky"
+        justifyContent="center"
+        w="120px"
+        h="full"
+        px="10px"
+        py="30px"
+        borderRadius="0px 25px 25px 0px"
+        bgColor="wheat"
+      >
+        <VStack>
+          <Text>User:</Text>
+          <Text>{user?.email}</Text>
+        </VStack>
+
+        <Link href="/api/auth/logout">Logout</Link>
+      </VStack>
+      {children}
     </HStack>
-    {children}
-  </HStack>
-)
+  )
+}
