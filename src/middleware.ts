bc530a66e5dc04c79cfa9f53bdf4f0ad3d9064af
Martin Mroč <mato.mroc@gmail.com>
2024-11-22 11:49:55 +0100

    remove middleware functionality

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,48 +1,48 @@
-import type { NextRequest } from 'next/server'
-import { NextResponse } from 'next/server'
+// import type { NextRequest } from 'next/server'
+// import { NextResponse } from 'next/server'
 
-const verifyBasicAuth = (authHeader: string | null, username: string, password: string) => {
-  if (!authHeader) {
-    return false
-  }
+// const verifyBasicAuth = (authHeader: string | null, username: string, password: string) => {
+//   if (!authHeader) {
+//     return false
+//   }
 
-  const base64Credentials = authHeader.split(' ')[1]
-  const credentials = Buffer.from(base64Credentials, 'base64').toString('ascii')
-  const [providedUsername, providedPassword] = credentials.split(':')
+//   const base64Credentials = authHeader.split(' ')[1]
+//   const credentials = Buffer.from(base64Credentials, 'base64').toString('ascii')
+//   const [providedUsername, providedPassword] = credentials.split(':')
 
-  return providedUsername === username && providedPassword === password
-}
+//   return providedUsername === username && providedPassword === password
+// }
 
-export const middleware = (request: NextRequest) => {
-  // Check for preview deployment
-  const isPreview = process.env.VERCEL_ENV === 'preview'
+// export const middleware = (request: NextRequest) => {
+//   // Check for preview deployment
+//   const isPreview = process.env.VERCEL_ENV === 'preview'
 
-  if (isPreview) {
-    const authHeader = request.headers.get('authorization')
+//   if (isPreview) {
+//     const authHeader = request.headers.get('authorization')
 
-    // If credentials are wrong, get back auth request
-    if (
-      !verifyBasicAuth(
-        authHeader,
-        process.env.PREVIEW_USERNAME || '',
-        process.env.PREVIEW_PASSWORD || ''
-      )
-    ) {
-      return new NextResponse('Authentication required', {
-        status: 401,
-        headers: {
-          'WWW-Authenticate': 'Basic realm="Preview deployment"',
-        },
-      })
-    }
-  }
-  return NextResponse.next()
-}
+//     // If credentials are wrong, get back auth request
+//     if (
+//       !verifyBasicAuth(
+//         authHeader,
+//         process.env.PREVIEW_USERNAME || '',
+//         process.env.PREVIEW_PASSWORD || ''
+//       )
+//     ) {
+//       return new NextResponse('Authentication required', {
+//         status: 401,
+//         headers: {
+//           'WWW-Authenticate': 'Basic realm="Preview deployment"',
+//         },
+//       })
+//     }
+//   }
+//   return NextResponse.next()
+// }
 
-// Definition where should be middleware applied
-export const config = {
-  matcher: [
-    // Apply for every routes except API routes and static files
-    '/((?!api|_next/static|_next/image|favicon.ico).*)',
-  ],
-}
+// // Definition where should be middleware applied
+// export const config = {
+//   matcher: [
+//     // Apply for every routes except API routes and static files
+//     '/((?!api|_next/static|_next/image|favicon.ico).*)',
+//   ],
+// }
